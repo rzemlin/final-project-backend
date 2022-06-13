d7eec744c857212ab1b1c9608ac7fa9f4f56fcea
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-06-13 14:28:30 -0700

    add model relationships and migrate db

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  belongs_to :user, optional: true
+  belongs_to :post, dependant: destroy
 end
--- db/migrate/20220613211133_create_posts.rb
+++ db/migrate/20220613211133_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.string :image
       t.string :caption
       t.integer :likes
-
+      t.integer :user_id
       t.timestamps
     end
   end
