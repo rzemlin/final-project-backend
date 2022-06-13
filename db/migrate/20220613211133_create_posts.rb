f3404d43804304876dcf4c3f86cf192dd175caa8
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-06-13 14:16:57 -0700

    add user, post, and comment models

---
+++ db/migrate/20220613211133_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :image
+      t.string :caption
+      t.integer :likes
+
+      t.timestamps
+    end
+  end
+end
