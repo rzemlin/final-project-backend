b9dc57d9230bf98eaddb7ad65e29a5011ebbfeb8
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-06-16 06:47:09 -0700

    add minimal routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api: do
+    namespace :v1 do
+      resources :users, only: [:create, :destroy]
+      resources :posts, only: [:index, :create, :destroy]
+      resources :comments, only: [:create, :destroy]
+      get '/posts', to: "posts#index"
 end
