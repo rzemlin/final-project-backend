0510763d77c0a05221584419ae010ceffdd16895
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-06-17 11:44:27 -0700

    update crud actions in post controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,44 @@
 class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only: [:update, :destroy]
+
   def index
     posts = Post.all
     render json: PostSerializer.new(posts), :except => [:created_at, :updated_at]
   end
+end
+
+def create
+  post = logged_in_user.posts.build(post_params)
+  if post.save
+    render json: post, status: :created, location: post
+  else
+    render json: post.errors, status: :unprocessable_entity
+  end
+end
+
+def show
+  post = Post.find(params[:id])
+  render json: post, serializer: PostSerializer
+end
+
+def update
+  if post.update(post_params)
+    render json: post
+  else
+    render json: post.errors, status: :unprocessable_entity
+  end
+end
+
+def destroy
+  post.destroy
+end
+
+private
+
+def set_post
+  post = Post.find(params[:id])
+end
+
+def post_params
+  params.permit(:image, :likes, :caption)
 end
