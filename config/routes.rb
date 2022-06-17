555492453e2fedd1a5a836aceaafb04653ac8a36
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-06-17 11:53:09 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :destroy]
+      resources :users, only: [:index, :show]
       resources :posts, only: [:index, :create, :destroy]
-      resources :comments, only: [:create, :destroy]
+      resources :comments, only: [:create]
       get "/posts", to: "posts#index"
+      post "/signup", to: "users#create"
+      get "/profile", to: "users#show"
+      post "/login", to: "sessions#login"
+      get "hello", to: "sessions#autologin"
     end
   end
 end
